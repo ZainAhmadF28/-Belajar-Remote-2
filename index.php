3ce48723e0bc34485f721c13ae5ef85dfb54e074
ZAIN AHMAD FAHREZI <2023110101@students.uigm.ac.id>
2024-09-11 21:47:31 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,17 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, vitae. Soluta ipsa dolor quo, voluptates excepturi repudiandae fugit, ut veniam error voluptatem expedita velit voluptas aut natus dolorum, officia quia.</p>
+    <ul>
+        <li>Zain</li>
+        <li>AGP</li>
+        <li>Adhe</li>
+        <li>Rein</li>
+        <li>Syafa</li>
+        <li>Jaki</li>
+        <li>Sayyida</li>
+        <li>Nyimas</li>
+        <li>Sindi</li>
+        <li>Ajeng</li>
+    </ul>
 </body>
 </html>
