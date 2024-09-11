2e3e4e6233a49e4391482234c69ec333cad67c59
ZAIN AHMAD FAHREZI <2023110101@students.uigm.ac.id>
2024-09-11 21:39:42 +0700

    Menambahkan Paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, vitae. Soluta ipsa dolor quo, voluptates excepturi repudiandae fugit, ut veniam error voluptatem expedita velit voluptas aut natus dolorum, officia quia.</p>
 </body>
 </html>
